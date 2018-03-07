d2733e67881ff0102c4a78aed444caa4ae21deea
Neil MartinsenBurrell <neil@moz.com>
2018-03-06 21:05:03 -0600

    Configure SQLAlchemy and print some db info

--- app.py
+++ app.py
@@ -5,8 +5,10 @@ from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 db = SQLAlchemy(app)
 
 @app.route('/')
 def home():
-    return "I am alive."
+    return "I am alive using" + str(db)
